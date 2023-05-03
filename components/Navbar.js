1ec00ee0de730a4a6f55d57ced2c0b60712044a5
Nesha Mervin <nesha.mervin@gmail.com>
2023-05-02 23:55:52 -0400

    updated nav's background colour and border colour

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,9 +3,9 @@ import { faBars, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
   return (
-    <nav className="text-white bg-greyBlack h-[90px] flex justify-between items-center border-b-[1px] border-gray-400 px-6 py-8">
+    <nav className="text-white bg-[#1b1b1b] h-[90px] flex justify-between items-center border-b-[1px] border-[#474747] px-6 py-8">
       <FontAwesomeIcon icon={faBars} className="w-5"></FontAwesomeIcon>
-      <span className="font-poppins text-2xl font-semibold tracking-wide">
+      <span className="font-poppins text-3xl font-semibold tracking-wide">
         audiophile
       </span>
       <FontAwesomeIcon icon={faCartShopping} className="h-5"></FontAwesomeIcon>
